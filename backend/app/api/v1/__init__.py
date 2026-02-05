2467754773948873fa9b368e2432698d19b11df2
Ashwin Gnanasekar <agnanasekar3@gatech.edu>
2026-02-04 19:52:30 -0500

    feat: api routes initialization

---
+++ backend/app/api/v1/__init__.py
@@ -0,0 +1,18 @@
+"""
+API v1 router aggregation.
+"""
+
+from fastapi import APIRouter
+
+from app.api.v1 import auth
+
+api_router = APIRouter()
+
+api_router.include_router(auth.router)
+
+# Future routers will be included here:
+# api_router.include_router(licenses.router)
+# api_router.include_router(subscriptions.router)
+# api_router.include_router(leads.router)
+# api_router.include_router(admin.router)
+# api_router.include_router(webhooks.router)
