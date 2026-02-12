b85cc8c3a200d1f694384a82a6189b9d25fed890
Ashwin Gnanasekar <agnanasekar3@gatech.edu>
2026-02-11 19:43:23 -0500

    feat: added transaction in progress check

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -82,6 +82,9 @@ def run_migrations_online() -> None:
         with context.begin_transaction():
             context.run_migrations()
 
+        if connection.in_transaction():
+            connection.commit()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
