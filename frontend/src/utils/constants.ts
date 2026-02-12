dc18a23209a1c3183fc55d2d950aa8a3a5845ece
Ashwin Gnanasekar <agnanasekar3@gatech.edu>
2026-02-12 12:41:58 -0500

    feat: added auth error messages as constants

---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const AUTH_ERROR_MESSAGES = {
+  login: "Unable to sign in. Please check your email/password and try again.",
+  register: "Unable to create account. Please try again.",
+} as const;
