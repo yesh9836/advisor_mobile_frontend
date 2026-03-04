2599a7d2db08426cfa33ef45f7e40704744f60ea
Ashwin Gnanasekar <agnanasekar3@gatech.edu>
2026-03-04 18:04:31 -0500

    feat: sentry initalization

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom/client";
 import App from "@/App";
 import "@/index.css";
 import "@/styles/app-shell.css";
+import { initSentry } from "@/lib/sentry";
+
+initSentry();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
